bbe55c2e9673e9079c34babb9c539864f699cafd
Leszek Wierzchleyski <66893940+Leszek-Wierzchleyski@users.noreply.github.com>
2020-09-15 23:15:36 +0100

    Update Vector-Field-Trace.py

--- Vector-Field-Trace.py
+++ Vector-Field-Trace.py
@@ -4,12 +4,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def VectorFieldTrace2D(fgrid, x0, y0, h, t0, tf):
+def VectorIntLin(fgrid, xp):
     
+    """Interpolates between two points on a one day grud to find the value of the function at the point xp."""
+    
+    x1 = int(np.floor(xp))
+
+    x2 = int(np.ceil(xp))
+
+    if xp - x1 == 0:
+        x1 = int(xp - 1)
+        x2 = int(xp + 1)
+
+    p = fgrid[x1] + (((fgrid[x2] - fgrid[x1]) / (x2 - x1)) * (xp - x1))
+
+    return p
+
+
+
+def VectorFieldTrace2D(fgrid1, fgrid2, x0, y0, h, t0, tf):
+
     """Traces a vector field on a 2D grid where the value of the function is known at the vertices on the grid. Starts
-    from a position (x0,y0) and traces the vector field for a given time period tf-t0, for a given step size h. 
-    fgrid must be a 2D numpy array"""
-    
+    from a position (x0,y0) and traces the vector field for a given time period tf-t0, for a given step size h."""
+
     N = int((tf - t0) / h)
 
     t = np.linspace(t0, tf, N + 1)
@@ -24,29 +41,34 @@ def VectorFieldTrace2D(fgrid, x0, y0, h, t0, tf):
 
 
     for i in range(N):
-        k1 = VectorInt2D(fgrid, x, y)
-        xn1 = x + 0.5 * h * k1
-        yn1 = y + 0.5 * h * k1
-        k2 = VectorInt2D(fgrid, xn1, yn1)
-        xn2 = x + 0.5 * h * k2
-        yn2 = y + 0.5 * h * k2
-        k3 = VectorInt2D(fgrid, xn2, yn2)
-        xn3 = x + h * k3
-        yn3 = y + h * k3
-        k4 = VectorInt2D(fgrid, xn3, yn3)
-        x = x + (1 / 6) * h * (k1 + 2 * k2 + 2 * k3 + k4)
-        y = y + (1 / 6) * h * (k1 + 2 * k2 + 2 * k3 + k4)
+        k1x = VectorIntLin(fgrid1, x)
+        k1y = VectorIntLin(fgrid2, y)
+        xn1 = x + 0.5 * h * k1x
+        yn1 = y + 0.5 * h * k1y
+        k2x = VectorIntLin(fgrid1, xn1)
+        k2y = VectorIntLin(fgrid2, yn1)
+        xn2 = x + 0.5 * h * k2x
+        yn2 = y + 0.5 * h * k2y
+        k3x = VectorIntLin(fgrid1, xn2)
+        k3y = VectorIntLin(fgrid2, yn2)
+        xn3 = x + h * k3x
+        yn3 = y + h * k3y
+        k4x = VectorIntLin(fgrid1, xn3)
+        k4y = VectorIntLin(fgrid2, yn3)
+        x = x + (1 / 6) * h * (k1x + 2 * k2x + 2 * k3x + k4x)
+        y = y + (1 / 6) * h * (k1y + 2 * k2y + 2 * k3y + k4y)
         xn.append(x)
         yn.append(y)
-        u = xn[i] - xn[i-1]
-        v = yn[i] - yn[i - 1]
+        u = x
+        v = y
     fig, ax = plt.subplots()
     q = ax.quiver(xn, yn, u, v, color='blue')
     plt.grid()
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
-    return xn, yn
+    print(xn)
+    print(yn)
 
 def VectorFieldTrace3D(fgrid, x0, y0, z0, h, t0, tf):
     """Traces a vector field on a 3D lattice where the value of the function is known at the vertices on the lattice. 
